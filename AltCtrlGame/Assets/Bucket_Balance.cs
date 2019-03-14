d56c7e0ff08adc8b915ac8aa1a5156ae567fe7a0
samuelhegner <C16439262@mydit.ie>
2019-03-14 19:29:58 +0000

    More Tweeks

--- AltCtrlGame/Assets/Bucket_Balance.cs
+++ AltCtrlGame/Assets/Bucket_Balance.cs
@@ -18,6 +18,8 @@ public class Bucket_Balance : MonoBehaviour
     float noiseScroller = 0;
     public float noiseInc;
 
+    public RectTransform arrow;
+
     
     void Awake()
     {
@@ -30,22 +32,29 @@ public class Bucket_Balance : MonoBehaviour
     void FixedUpdate()
     {
         noiseScroller += noiseInc;
-        print((Mathf.PerlinNoise(randomNoiseStartX + noiseScroller, randomNoiseStartY) * 2f) - 1f);
-        AddRandomForce((Mathf.PerlinNoise(randomNoiseStartX + noiseScroller, randomNoiseStartY) * 2f) - 1f);
+        print((Mathf.PerlinNoise(randomNoiseStartX + noiseScroller, randomNoiseStartY + noiseScroller) * 2f) - 1f + " : " + noiseScroller);
+        AddRandomForce((Mathf.PerlinNoise(randomNoiseStartX + noiseScroller, randomNoiseStartY + noiseScroller) * 2f) - 1f);
         AddForce();
+        SetTiltOmeter((Mathf.PerlinNoise(randomNoiseStartX + noiseScroller, randomNoiseStartY + noiseScroller) * 2f) - 1f);
     }
 
     void AddForce() {
         float hAxis = Input.GetAxis("Horizontal");
         Vector3 forceVector = transform.TransformPoint(new Vector3(hAxis * force, 0, 0));
 
-        rb.AddForce(forceVector, ForceMode.Acceleration);
+        rb.AddForce(forceVector, ForceMode.Force);
     }
 
     void AddRandomForce(float NoiseVal)
     {
         NoiseVal *= noiseScale;
         Vector3 forceVector = transform.TransformPoint(new Vector3(NoiseVal * randomForce, 0, 0));
-        rb.AddForce(forceVector, ForceMode.Acceleration);
+        rb.AddForce(forceVector, ForceMode.Force);
+    }
+
+    void SetTiltOmeter(float NoiseVal) {
+
+        float zRot = Game_Manager.Map(NoiseVal, -1f, 1f, 89f, -89f);
+        arrow.rotation = Quaternion.Euler(new Vector3(0, 0, zRot));
     }
 }
---
+++ AltCtrlGame/Assets/Game_Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static float Map(float a, float b, float c, float d, float e)
+    {
+        float cb = c - b;
+        float de = e - d;
+        float howFar = (a - b) / cb;
+        return d + howFar * de;
+    }
+}
