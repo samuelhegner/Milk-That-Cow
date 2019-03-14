1110bfc1117087cd3376c357648e8879a5f1cbf9
samuelhegner <C16439262@mydit.ie>
2019-03-14 14:21:49 +0000

    Balancing Start

---
+++ AltCtrlGame/Assets/Bucket_Balance.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket_Balance : MonoBehaviour
+{
+    public float force;
+
+    Rigidbody rb;
+
+    float randomNoiseStartX;
+    float randomNoiseStartY;
+
+    float noiseScroller = 0;
+    public float noiseInc;
+
+    public float randomForce;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        randomNoiseStartX = Random.Range(-1000, 1000);
+        randomNoiseStartY = Random.Range(-1000, 1000);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        noiseScroller += noiseInc;
+        AddRandomForce();
+        AddForce();
+    }
+
+    void AddForce() {
+        float hAxis = Input.GetAxis("Horizontal");
+        Vector3 forceVector = transform.TransformPoint(new Vector3(hAxis * force, 0, 0));
+
+        rb.AddForce(forceVector, ForceMode.Force);
+    }
+
+    void AddRandomForce() {
+        float noiseValue = Mathf.PerlinNoise(randomNoiseStartX + noiseScroller, randomNoiseStartY) * 2 - 1;
+
+        print(noiseValue);
+
+        Vector3 forceVector = transform.TransformPoint(new Vector3(noiseValue * randomForce, 0, 0));
+        rb.AddForce(forceVector, ForceMode.Force);
+    }
+}
---
+++ AltCtrlGame/Assets/Noise_Value_Tester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise_Value_Tester : MonoBehaviour
+{
+
+    float noiseScroller = 0;
+    public float noiseInc = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        noiseScroller += noiseInc * Time.deltaTime;
+        float noiseValue = Mathf.PerlinNoise(0 + noiseScroller, 0) * 2 - 1;
+
+        transform.position = new Vector3(0, noiseValue, 0);
+    }
+}
