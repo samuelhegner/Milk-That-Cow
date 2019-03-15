a318cec2637fd8bc734d29539b5740aee51cf67c
Eoin Ahern <1413840+Prongt@users.noreply.github.com>
2019-03-15 21:02:51 +0000

    Testing nvidia flex

---
+++ AltCtrlGame/Assets/Scripts/ParticleCollisionTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class ParticleCollisionTest : MonoBehaviour
+{
+    public int milkCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Particle Collision");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //return;
+        Debug.Log("Particle trigger" + col.name);
+        Destroy(col.gameObject);
+        milkCount++;
+        Debug.Log(milkCount);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Particle Collision " + col.gameObject.name);
+        Destroy(col.gameObject);
+        milkCount++;
+        Debug.Log(milkCount);
+    }
+}
