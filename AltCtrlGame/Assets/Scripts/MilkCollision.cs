eb0ec3757c72368e42198c56c0d271b727fa9119
Eoin Ahern <eahern123@gmail.com>
2019-03-20 16:03:48 +0000

    Started work on the bucket disabling

--- AltCtrlGame/Assets/Scripts/MilkCollision.cs
+++ AltCtrlGame/Assets/Scripts/MilkCollision.cs
@@ -1,13 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MilkCollision : MonoBehaviour
 {
     private MilkSystem milkSystem;
     public bool DontRegisterParticle = false;
+
+
+    private static List<GameObject> MilkColliders;
+
+    private void Awake()
+    {
+        MilkColliders = new List<GameObject>();
+    }
+
     private void Start()
     {
         milkSystem = GameObject.FindGameObjectWithTag("MilkManager").GetComponent<MilkSystem>();
-        if (gameObject.CompareTag("Untagged")) Debug.LogWarning(gameObject.name + " is not tagged with a team tag");
+        if (gameObject.CompareTag("Untagged"))
+        {
+            Debug.LogWarning(gameObject.name + " is not tagged with a team tag");
+        }
+
+        if (!DontRegisterParticle)
+        {
+            MilkColliders.Add(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -19,14 +37,14 @@ public class MilkCollision : MonoBehaviour
             if (DontRegisterParticle)
             {
                 col.GetComponent<MetaballParticleClass>().SetTrailRenderer = false;
-                col.gameObject.transform.SetParent(this.transform);
+                col.gameObject.transform.SetParent(transform);
                 Debug.Log("ParentChanged");
                 return;
             }
             Debug.Log("Milk detected add score " + col.tag);
             milkSystem.AddOrRemoveMilk(gameObject.tag, true);
 
-            
+
             //col.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         }
         else
@@ -50,8 +68,25 @@ public class MilkCollision : MonoBehaviour
             Debug.Log("Milk leaving remove score");
             milkSystem.AddOrRemoveMilk(gameObject.tag, false);
 
-            
+
             //col.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.Discrete;
         }
     }
+
+
+    public static GameObject DisableBucket(string teamTag)
+    {
+        GameObject objToReturn = null;
+        foreach (var obj in MilkColliders)
+        {
+            if (obj.tag == teamTag)
+            {
+                obj.SetActive(false);
+                objToReturn = obj;
+            }
+        }
+
+        return objToReturn;
+    }
+
 }
