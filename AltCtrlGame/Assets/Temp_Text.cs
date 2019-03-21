faaa58d1363f4b29263062ae9152a131646e73c4
samuelhegner <C16439262@mydit.ie>
2019-03-21 12:53:23 +0000

    Tmp and some basic text stuff

---
+++ AltCtrlGame/Assets/Temp_Text.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Temp_Text : MonoBehaviour
+{
+    bool fullSize;
+
+    public float lerpSpeed;
+    public float targetScale;
+
+    TextMeshProUGUI tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = GetComponent<TextMeshProUGUI>();
+        transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!fullSize)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(targetScale, targetScale, targetScale), Time.deltaTime * lerpSpeed);
+            if (Vector3.Distance(transform.localScale, new Vector3(targetScale, targetScale, targetScale)) < 0.05f)
+            {
+                fullSize = true;
+            }
+        }
+        else {
+            tmp.color = Color.Lerp(tmp.color, new Color(tmp.color.r, tmp.color.g, tmp.color.b, 0), Time.deltaTime * lerpSpeed * 2f);
+
+            if (tmp.color.a < 0.01f) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
