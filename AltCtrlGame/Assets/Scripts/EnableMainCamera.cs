89178d175a6fc7fdc1d5a578e2e01eb218227772
gazor117 <oreillygary7@gmail.com>
2019-03-23 21:18:19 +0000

    Main camera enabled

---
+++ AltCtrlGame/Assets/Scripts/EnableMainCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableMainCamera : MonoBehaviour
+{
+    public float IntroTime;
+    public GameObject MainCameras;
+    public GameObject introCamera;
+    private bool cameraNotActivated = true;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnableMainCameras());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator EnableMainCameras()
+    {
+        while (cameraNotActivated)
+        {
+            yield return new WaitForSeconds(IntroTime);
+
+            MainCameras.SetActive(true);
+            introCamera.SetActive(true);
+            cameraNotActivated = false;
+        }
+
+    }
+}
