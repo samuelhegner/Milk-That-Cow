ae4b4a9126e78b69b4ef8d58ab2e19b5659cd692
PKdude96 <C16452312@mydit.ie>
2019-03-25 04:32:51 +0000

    Upload of Models

---
+++ AltCtrlGame/Assets/Patrick/Scripts/MilkBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkBar : MonoBehaviour
+{
+    private Transform bar;
+
+    private void Start()
+    {
+    bar = transform.Find("Bar"); //Finding reference to the milk bar's transform
+        bar.localScale = new Vector3(.4f, 1f);
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+//        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+
+}
---
+++ AltCtrlGame/Assets/Patrick/Scripts/MilkBarHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkBarHandler : MonoBehaviour
+{
+    [SerializeField] private MilkBar milkBar;
+
+    private void Start()
+    {
+//        milkBar.SetSize(.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
