65afce2b2445848d56642f64886e7a5df9c2069f
Eoin Ahern <1413840+Prongt@users.noreply.github.com>
2019-03-28 21:34:44 +0000

    Added start scene and intro only plays once

---
+++ AltCtrlGame/Assets/MilkSceneChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MilkSceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;
+    public string inputAxis1 = "Milk1_Vertical";
+    public string inputAxis2 = "Milk2_Vertical";
+    private bool SceneJustLoaded = false;
+
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetAxis(inputAxis1) < -0.8f || Input.GetAxis(inputAxis2) < -0.8f || Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        SceneManager.LoadScene(sceneToLoad);
+    //    }
+    //}
+
+    IEnumerator LoadScene()
+    {
+        while (true)
+        {
+            if (!SceneJustLoaded)
+            {
+                SceneJustLoaded = true;
+                yield return new WaitForSeconds(1f);
+            }
+
+            if (Input.GetAxis(inputAxis1) < -0.8f || Input.GetAxis(inputAxis2) < -0.8f || Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
