14cf3f2d49c31d1a55ac880f586a087d83928308
Eoin Ahern <1413840+Prongt@users.noreply.github.com>
2019-03-28 16:08:25 +0000

    Updated score stuff

--- AltCtrlGame/Assets/Scripts/DisplayScore.cs
+++ AltCtrlGame/Assets/Scripts/DisplayScore.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class DisplayScore : MonoBehaviour
 {
@@ -23,12 +24,18 @@ public class DisplayScore : MonoBehaviour
     private void SetText(UIFields data)
     {
         var scoreData = ScoreManager.GetScoreData(data.teamTag);
+        scoreData.bucketsDrank = Random.Range(1, 15);
+        //Debug.Log(scoreData.milkSplit);
+        //Debug.Log(scoreData.milkProduced);
+        float milkSpilt = ((float)scoreData.milkSplit / (float)scoreData.milkProduced) * 100;
+        float total = scoreData.bucketsDrank * (scoreData.milkProduced - scoreData.milkSplit);
+        //Debug.Log(total);
         data.teamNameText.text = data.teamName;
         data.bucketsDrankText.text = "Buckets drank " + scoreData.bucketsDrank;
-        data.milkSpiltText.text = "Milk split " + scoreData.milkSplit;
-        data.milkProducedText.text = "Milk produced " + scoreData.milkProduced;
-        data.milkCaughtInBucketText.text = "Milk caught " + scoreData.milkCaughtInBucket;
-        data.totalScoreText.text = "Total score " + scoreData.totalScore;
+        data.milkSpiltText.text = "Milk split " + milkSpilt + " %";
+        //data.milkProducedText.text = "Milk produced " + scoreData.milkProduced;
+        //data.milkCaughtInBucketText.text = "Milk caught " + scoreData.milkCaughtInBucket;
+        data.totalScoreText.text = "Total score " + total;
     }
 
     [Serializable]
@@ -39,8 +46,8 @@ public class DisplayScore : MonoBehaviour
         public Text teamNameText;
         public Text bucketsDrankText;
         public Text milkSpiltText;
-        public Text milkProducedText;
-        public Text milkCaughtInBucketText;
+        //public Text milkProducedText;
+        //public Text milkCaughtInBucketText;
         public Text totalScoreText;
     }
 }
